94e0502dd3704ca599f347a981bce077ff0d8a62
Arne Mertz <arne.mertz@zuehlke.com>
2022-06-07 15:38:00 +0200

    refactor filter parsing

--- CppKata/Filters.cpp
+++ CppKata/Filters.cpp
@@ -5,32 +5,54 @@
 #include <regex>
 #include <string>
 
-Filters Filters::parse(std::istream& is)
-{
-	const std::regex filters_start{"filters: \\{"};
-	std::string line1;
-	while (std::getline(is, line1) && !std::regex_search(line1, filters_start))
-	{
-		continue;
-	}
+using FilterItems = std::map<std::string, std::string, std::less<>>;
 
-	std::string line;
-	const static std::regex filter_item_regex{ "(\\w+): (\\w+)" };
-	const std::regex filters_end{ "\\}" };
-	std::map<std::string, std::string> filter_items;
-	while (std::getline(is, line) && !std::regex_search(line, filters_end))
+namespace {
+	void ignoreLinesUntilFilterStart(std::istream& is)
 	{
-		if (std::smatch item_match; std::regex_search(line, item_match, filter_item_regex))
+		const std::regex filters_start{ "filters: \\{" };
+		std::string line1;
+		while (std::getline(is, line1) && !std::regex_search(line1, filters_start))
 		{
-			filter_items.emplace(item_match[1], item_match[2]);
+			continue;
 		}
 	}
 
-	Filters filters;
-	if (const auto pos = filter_items.find("minItemCount"); pos != end(filter_items))
+	auto parseFilterItems(std::istream& is)
 	{
-		filters.minItemCount = std::atoi(pos->second.c_str());
+		const std::regex filter_item_regex{ "(\\w+): (\\w+)" };
+		const std::regex filters_end{ "\\}" };
+
+		FilterItems filter_items;
+		std::string line;
+		while (std::getline(is, line) && !std::regex_search(line, filters_end))
+		{
+			if (std::smatch item_match; std::regex_search(line, item_match, filter_item_regex))
+			{
+				filter_items.emplace(item_match[1], item_match[2]);
+			}
+		}
+		return filter_items;
 	}
+
+	void setFilterFromItem(std::optional<unsigned>& filter, FilterItems const& filter_items, std::string_view const& name)
+	{
+		if (const auto pos = filter_items.find(name); pos != end(filter_items))
+		{
+			filter = std::atoi(pos->second.c_str());
+		}
+	}
+
+} // anonymous namespace
+
+
+Filters Filters::parse(std::istream& is)
+{
+	ignoreLinesUntilFilterStart(is);
+	const auto filter_items = parseFilterItems(is);
+
+	Filters filters;
+	setFilterFromItem(filters.minItemCount, filter_items, "minItemCount");
 	return filters;
 }
 
