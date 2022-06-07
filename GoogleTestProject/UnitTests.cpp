e4c12d4e539de51c4808ba5be169b366cc26e091
Arne Mertz <arne.mertz@zuehlke.com>
2022-06-07 14:14:14 +0200

    add test for complete package filter kata

--- GoogleTestProject/UnitTests.cpp
+++ GoogleTestProject/UnitTests.cpp
@@ -5,13 +5,33 @@
 #include <sstream>
 
 using namespace std::literals;
-TEST(process, prints_hello_world)
+TEST(package_filter, DISABLED_readme_example_works)
 {
-	const auto sample_input =
-		"line 1\n"
-		"other line\n"
-		"line 3\n"
-	""s;
-	std::stringstream sstr{sample_input};
-	ASSERT_EQ(process(sstr), "lol"s);
+	const auto readme_input =
+R"(filters: {
+    minPackageWeight: "2kg",
+    maxPackageWeight: "15kg",
+    minItemWeight: "200g",
+    maxItemWeight: "5kg",
+    minItemCount: 2,
+    maxItemCount: 50,
+    minPackageCount: 1,
+    maxPackageCount: 50
+},
+packages: [
+    {itemName: "screwDriver", itemWeight: "400g", itemCount: 5, packageCount: 12},
+    {itemName: "powerDrill", itemWeight: "2500g", packageCount: 5},
+    {itemName: "screw", itemCount: 500, packageCount: 10, packageWeight: "500g"},
+    {itemID: "DA456-32F", packageWeight: "4000g", itemWeight: "800g"},
+    {itemWeight: "800g", itemCount: 6, packageWeight: "5kg", packageCount: 2, itemID: "FIA-77-g64x"},
+    {itemCount: 20, itemName: "gummy bears", packageCount: 50},
+    {itemCount: 10, itemWeight: "2300g", itemName: "powerDrill", powerUse="1400W"}
+]
+)"s;
+
+	const auto readme_output =
+R"(totalItemCount: 73, totalPackageCount: 15, totalWeight: "38000g")"s;
+
+	std::stringstream sstr{ readme_input };
+	ASSERT_EQ(process(sstr), readme_output);
 }
