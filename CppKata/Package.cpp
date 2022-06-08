f5e398813c762bbd165a1502a7209962436b85f7
Arne Mertz <arne.mertz@zuehlke.com>
2022-06-08 15:31:41 +0200

    refactor package parsing

--- CppKata/Package.cpp
+++ CppKata/Package.cpp
@@ -26,6 +26,31 @@ namespace
 		}
 	}
 
+	PackageAttributes extractPackageAttributes(std::string const& line)
+	{
+		const auto start = std::find(line.begin(), line.end(), '{');
+		const auto end = std::find(start, line.end(), '}');
+		const auto key_value_regex = std::regex{ "(\\w+): (\\w+)" };
+
+		PackageAttributes package_attributes;
+		for (auto rit = std::sregex_iterator{ start, end, key_value_regex }; rit != std::sregex_iterator{}; ++rit)
+		{
+			const auto match = *rit;
+			package_attributes.emplace(match[1], match[2]);
+		}
+		return package_attributes;
+	}
+
+	Package parsePackage(std::string const& line)
+	{
+		const auto package_attributes = extractPackageAttributes(line);
+
+		Package package;
+		setPackageAttr(package.itemCount, package_attributes, "itemCount");
+		setPackageAttr(package.packageCount, package_attributes, "packageCount");
+		return package;
+	}
+
 } // anonymous namespace
 
 
@@ -37,20 +62,7 @@ std::vector<Package> Package::parse(std::istream& is)
 	std::vector<Package> packages;
 	while (std::getline(is, line) && (line.find(']') == std::string::npos))
 	{
-		const auto start = std::find(line.begin(), line.end(), '{');
-		const auto end = std::find(start, line.end(), '}');
-		const auto key_value_regex = std::regex{"(\\w+): (\\w+)"};
-		PackageAttributes package_attributes;
-		for (auto rit = std::sregex_iterator{start, end, key_value_regex}; rit != std::sregex_iterator{}; ++rit)
-		{
-			const auto match = *rit;
-			package_attributes.emplace(match[1], match[2]);
-		}
-
-		Package package;
-		setPackageAttr(package.itemCount, package_attributes, "itemCount");
-		setPackageAttr(package.packageCount, package_attributes, "packageCount");
-		packages.push_back(package);
+		packages.emplace_back(parsePackage(line));
 	}
 
 	return packages;
